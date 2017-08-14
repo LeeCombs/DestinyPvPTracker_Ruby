a2be94c83cfdbae73cef3d249b4035c473383768
HellaBored <leecombs_30@hotmail.com>
2017-08-14 12:19:10 -0700

    Randomize seeding a bit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Character.create!(id: 1, user_id: "123", scout: 111, hand: 222, pulse: 333, auto: 444, sniper: 555, shotgun: 666, fusion: 777, sidearm: 888)
-Character.create!(id: 2, user_id: "456", scout: 555, hand: 666, pulse: 777, auto: 888, sniper: 999, shotgun: 111, fusion: 222)
+Character.create!(id: 1, user_id: "123", 
+                  scout: rand(1000), hand: rand(1000), pulse: rand(1000), auto: rand(1000), 
+                  sniper: rand(1000), shotgun: rand(1000), fusion: rand(1000), sidearm: rand(1000))
+Character.create!(id: 2, user_id: "456", 
+                  scout: rand(1000), hand: rand(1000), pulse: rand(1000), auto: rand(1000), 
+                  sniper: rand(1000), shotgun: rand(1000), fusion: rand(1000), sidearm: rand(1000))
