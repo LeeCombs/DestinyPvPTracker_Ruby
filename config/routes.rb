c97e87361969631420056635d1950fb01709bcd7
HellaBored <leecombs_30@hotmail.com>
2017-08-14 10:12:41 -0700

    Swapped out forms, updated character show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/compare', to: 'compare#index'
 
+  get '/character', to: 'characters#show'
+
   resources :characters
 end
