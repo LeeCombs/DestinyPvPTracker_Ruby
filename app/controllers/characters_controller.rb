95541bba1046affc0440530b656e2cd8992bccc1
HellaBored <leecombs_30@hotmail.com>
2017-08-18 19:05:01 -0700

    Tidying before branch switch

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,21 +5,19 @@ class CharactersController < ApplicationController
 
     # Make the http requets to get player information
     res = SearchPlayerName(params[:displayName])
-    resBody = JSON.parse res.body
 
-    puts 'spn res'
-    puts res
     if res['Response'].blank?
+      puts "Player not found"
       return
     end
-    puts 'res body'
-    puts resBody
+
+    resBody = JSON.parse res.body
 
     # Grab or create the player's record
     @player = DestinyPlayer.find_by membership_id: resBody["Response"][0]["membershipId"]
     if @player.nil?
       puts 'Creating player record for: ' + params[:displayName]
-      @player = DestinyPlayer.new
+      @player = DestinyPlayer.new(user_params)
       @player.membership_id   = resBody["Response"][0]["membershipId"]
       @player.membership_type = resBody["Response"][0]["membershipType"]
       @player.display_name    = resBody["Response"][0]["displayName"]
@@ -31,13 +29,6 @@ class CharactersController < ApplicationController
 
   def new
     @player = DestinyPlayer.new
-  end
-
-  def create
-    @player = DestinyPlayer.new(user_params)
-    @player.save
-    puts "player saved"
-    redirect_to @player
   end
 
   private
